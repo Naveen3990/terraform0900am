94791c2e3a69c9d519ce5b5adbbc7691d529db09
Naveen3990 <Sharmivarun3990@gmail.com>
2026-06-23 17:20:34 +0530

    jenkin

---
+++ Jenkin-ec2-creation/main.tf
@@ -0,0 +1,20 @@
+# Get latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "terraform-ec2-instance"
+  }
+}
